645fe3a840b6393755dc3dfcb017150ea7a98278
ASGOOD1 <alexjoaca034@gmail.com>
2025-04-25 12:33:59 +0300

    Started view all orders activity on Admin Menu

--- app/src/main/java/com/example/pdm/ViewOrders.kt
+++ app/src/main/java/com/example/pdm/ViewOrders.kt
@@ -1,33 +1,20 @@
 package com.example.pdm
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class AdminMenuActivity : AppCompatActivity() {
+class ViewOrders : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_admin_menu)
+        setContentView(R.layout.activity_view_orders)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnEditUser = findViewById<Button>(R.id.editUserButton)
-        btnEditUser.setOnClickListener {
-            val intent = Intent(this, EditUserActivity::class.java)
-            startActivity(intent)
-        }
-        val btnViewOrders = findViewById<Button>(R.id.buttonViewCommands)
-        btnViewOrders.setOnClickListener {
-            val intent = Intent(this, ViewOrders::class.java)
-            startActivity(intent)
         }
     }
 }
