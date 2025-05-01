96c3030929ccc3417668748bc6f1b786cf6f2458
ASGOOD1 <alexjoaca034@gmail.com>
2025-05-01 11:01:32 +0300

    Started edit products activity

---
+++ app/src/main/java/com/example/pdm/EditProductsActivity.kt
@@ -0,0 +1,29 @@
+/*TODO: Edit products
+    - Spinner with suppliers and listview with products
+    - Product data underneath (can be updated or deleted by pressing a specific button on the list view)
+    - Can be completed directly and then saved by pressing save(Update existing)
+    - Product can be added by pressing + floating button(new)
+
+
+ */
+
+package com.example.pdm
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EditProductsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_edit_products)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
